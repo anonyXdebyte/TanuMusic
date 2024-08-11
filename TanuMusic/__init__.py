27d5b44f048539dafc657cf743d47a99172f9fba
GOD-R4V4N <kushwahaaditya9055l@gmail.com>
2024-08-11 19:54:49 +0530

    TanuMusic

---
+++ TanuMusic/__init__.py
@@ -0,0 +1,27 @@
+from TanuMusic.core.bot import Anony
+from TanuMusic.core.dir import dirr
+from TanuMusic.core.git import git
+from TanuMusic.core.userbot import Userbot
+from TanuMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Anony()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+YTB = YTM()
